794e8625d77314062ddd32dd0c6e9fd55c8642e5
paakkis <juusop2k12@gmail.com>
2024-04-12 01:09:21 +0300

    site descriptions

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,8 +3,8 @@
 </script>
 
 <svelte:head>
-	<title>Home</title>
-	<meta name="description" content="Svelte demo app" />
+	<title>Juuso Paakkunainen</title>
+	<meta name="description" content="A data person" />
 </svelte:head>
 
 <section>
